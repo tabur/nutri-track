932c5ea51c09b112ce62084659455cffbd832eda
Maria <54359868+ddmako@users.noreply.github.com>
2020-06-04 13:45:56 +0300

    Update app.js
    Lisäsin post toiminnon, toivottavasti se on jotain sinne päin

--- backend/app.js
+++ backend/app.js
@@ -49,6 +49,34 @@ app.delete("/api/food/:id",function(req,res) {
 	return res.status(404).json({message:"not found"})
 })
 
+app.post("/api/food",function(req,res) {
+        if(!req.body) {
+            return res.status(422).json({message:"provide required data"})
+        }
+        if(!req.body.firstname || !req.body.lastname) {
+            return res.status(422).json({message:"provide required data"})
+        }
+        if(req.body.firstname.length === 0 || req.body.lastname.length ===0) {
+            return res.status(422).json({message:"provide required data"})
+        }
+        let food = {
+            manufacturer:req.body.manufacturer,
+            description:req.body.description,
+            energy:req.body.energy,
+            carbs:req.body.carbs,
+            sugar:req.body.sugar,
+            fiber:req.body.fiber,
+            fat:req.body.fat,
+            saturated:req.body.saturated,
+            unsaturated:req.body.unsaturated,
+            protein:req.body.protein,
+            salt:req.body.salt,
+            id:id++
+        }
+        database.push(contact);
+        console.log(database);
+        return res.status(200).json({message:"success"})
+})
 
 //Laiton et tarvii olla energy ja manufacturer et voi muokata.
 app.put("/api/food/:id",function(req,res) {
