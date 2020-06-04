308f81e3e9eea1ee40aa2d51eb2b5c2284421adf
tabur <tabulae@gmail.com>
2020-06-04 12:26:07 +0300

    backend raakile ja ruoanlisays lomakkeen leiska

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const bodyParser = require("body-parser");
+
+//database
+
+let database = [];
+let id = 100;
+
+// initialization 
+
+let app = express();
+let port = process.env.PORT || 3001
+
+app.use(bodyParser.json());
+
+/*
+Data structure
+
+let food = {
+    manufacturer:String,
+    description:String,
+    energy:number,
+    carbs: number,
+    sugar: number,
+    fiber: number,
+    fat: number,
+    saturated: number,
+    unsaturated: number,
+    protein: number,
+    salt: number,
+}
+*/
+
+//REST API
+
+app.listen(port);
+
+console.log("Running in port:"+port);
