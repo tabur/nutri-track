2ae97adf9bd33e3559fec89c66c8cdf43788f15d
tabur <tabulae@gmail.com>
2020-06-06 12:20:36 +0300

    leiskan korjailua

--- frontend/src/components/AddFood.js
+++ frontend/src/components/AddFood.js
@@ -61,78 +61,84 @@ class AddFood extends React.Component {
           <hr />
 
           <legend>Nutrition per 100g</legend>
-        <Form.Row>
-          <Col>
-            <Row mb={2}>
-              <Form.Group controlId="Energy">
-                <Form.Label htmlFor="">Energy</Form.Label>
-                <Form.Control ml="auto" md={2} type="text" name="joules" id="joules" placeholder="Joules" />
-                <Form.Control ml={4} md={2} type="text" name="calories" id="calories" placeholder="Kcal" />
+          <Row>
+          <Col column md={4}>
+            <Row>
+              <Form.Group as={Row} controlId="Energy">
+                <Form.Label column md={4} htmlFor="">Energy</Form.Label>
+                <Col>
+                  <Form.Control ml="auto"  type="text" name="joules" id="joules" placeholder="Joules" />
+                  <Form.Control ml={4}  type="text" name="calories" id="calories" placeholder="Kcal" />
+                </Col>
+                
               </Form.Group>
             </Row>
             
             <fieldset className="carbset">
-              <Form.Row mb="2">
-                <Form.Group>
-                  <Form.Label htmlFor="" >Total Carbohydrates</Form.Label>
-                  <Form.Control ml="auto" md={2} type="text" name="carbs" id="carbs" placeholder="g" />
+
+                <Form.Group as={Row}>
+                  <Form.Label htmlFor="" column md={4}>Total Carbohydrates</Form.Label>
+                  <Col md={4}>
+                    <Form.Control ml="auto" type="text" name="carbs" id="carbs" placeholder="g" />
+                  </Col>
                 </Form.Group>
-              </Form.Row>
-              <Form.Row mb="2">
-                <Form.Group>
-                  <Form.Label pl={4} htmlFor="" >Sugar</Form.Label>
-                  <Form.Control ml="auto" md={2} type="text" name="sugar" id="sugar" placeholder="g" />
+
+              
+                <Form.Group as={Row}>
+                  <Form.Label pl={4} column md={4} htmlFor="" >Sugar</Form.Label>
+                  <Col md={4}>
+                    <Form.Control ml="auto" type="text" name="sugar" id="sugar" placeholder="g" />
+                  </Col>
                 </Form.Group>
-              </Form.Row>
-              <Form.Row mb="2">
-                <Form.Group>
-                  <Form.Label pl={4} htmlFor="">Fiber</Form.Label>
-                  <Form.Control ml="auto" md={2} type="text" name="fiber" id="fiber" placeholder="g" />
+              
+                <Form.Group as={Row}>
+                  <Form.Label pl={4} column md={4} htmlFor="">Fiber</Form.Label>
+                  <Col md={4}>
+                    <Form.Control ml="auto" type="text" name="fiber" id="fiber" placeholder="g" />
+                  </Col>
                 </Form.Group>
-              </Form.Row>
+
             </fieldset>
           </Col>
-          <Col md={4} ml={4}>
-            <Form.Row mb={2}>
-              <Form.Group>
-                <Form.Label htmlFor="">Protein</Form.Label>
-                <Form.Control ml="auto" md={2} type="text" name="protein" id="protein" placeholder="g" />
-              </Form.Group>
-            </Form.Row>
-            
-            
-            <fieldset className="fatset">
-              <Form.Row mb={2}>
-                <Form.Group>
-                  <Form.Label htmlFor="">Total Fat</Form.Label>
-                  <Form.Control ml="auto" md={2} type="text" name="fat" id="fat" placeholder="g" />
-                </Form.Group>
-              </Form.Row>
-              <Form.Row mb={2}>
-                <Form.Group>
-                  <Form.Label pl={4} htmlFor="">Saturated Fat</Form.Label>
-                  <Form.Control ml="auto" md={2} type="text" name="fat-sat" id="fat-sat" placeholder="g" />
-                </Form.Group>
-              </Form.Row>
-              <Form.Row mb={2}>
-                <Form.Group>
-                  <Form.Label pl={4} htmlFor="" >Unsaturated Fat</Form.Label>
-                  <Form.Control ml="auto" md={2} type="text" name="fat-unsat" id="fat-unsat" placeholder="g" />
-                </Form.Group>
-              </Form.Row>
-            </fieldset>
-               
-        </Col>
-      </Form.Row>            
-          <Form.Row>
-            <Form.Group pt={3}>
-              <Col md={10}>
-                <Button variant="success" type="submit" >Add new food</Button>
-                <Button variant="danger" type="cancel" >Cancel</Button>
+          <Col column md={4} ml={4}>
+            <Form.Group as={Row}>
+              <Form.Label column md={4} htmlFor="">Protein</Form.Label>
+              <Col>
+                <Form.Control ml="auto" type="text" name="protein" id="protein" placeholder="g" />
               </Col>
             </Form.Group>
-          </Form.Row>
-        
+
+            <fieldset className="fatset">
+              <Form.Group as={Row}>
+                <Form.Label column md={4} htmlFor="">Total Fat</Form.Label>
+                <Col>
+                  <Form.Control ml="auto" type="text" name="fat" id="fat" placeholder="g" />
+                </Col>
+              </Form.Group>
+
+              <Form.Group as={Row}>
+                <Form.Label column md={4} pl={4} htmlFor="">Saturated Fat</Form.Label>
+                <Col>
+                  <Form.Control ml="auto" type="text" name="fat-sat" id="fat-sat" placeholder="g" />
+                </Col>
+              </Form.Group>
+
+              <Form.Group as={Row}>
+                <Form.Label column md={4} pl={4} htmlFor="" >Unsaturated Fat</Form.Label>
+                <Col>
+                  <Form.Control ml="auto" type="text" name="fat-unsat" id="fat-unsat" placeholder="g" />
+                </Col>
+              </Form.Group>
+            </fieldset>
+               
+          </Col>
+        </Row>
+        <Form.Group as={Row} pt={3}>
+          <Col md={10}>
+            <Button variant="success" type="submit" >Add new food</Button>
+            <Button variant="danger" type="cancel" >Cancel</Button>
+          </Col>
+        </Form.Group>
         
         </Form> 
         
