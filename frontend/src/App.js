ed67b82af63b123f27a97a55299f47bbb95174d9
tabur <tabulae@gmail.com>
2020-06-16 13:06:14 +0300

    päivänäkymään aterianlisäys ja poisto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,22 +15,34 @@ class App extends React.Component {
 	constructor(props) {
     super(props);
 
+
+    let tempDate = new Date();
+
     // tilapäinen säilö
     this.state = {
-      list:[],
-      food:{},
+      foodList:[],
+			food:{},
+      loading:false,
       isLogged: false,
       username:"",
-      token: ""
-		}
+      token: "",
+      date: tempDate,
+      counter: 0,
+      mealList: []
+    }
+    this.getFoodList();
   }
 
+  //static date;
+
+	//meals: [date, amount, food{}]
 
 	//helpers
 	
 	loadFromStorage = () => {
 		if(sessionStorage.getItem("state")) {
-			let state = JSON.parse(sessionStorage.getItem("state"));
+      let state = JSON.parse(sessionStorage.getItem("state"));
+      state.date = new Date(state.date);
 			this.setState(state);
 		}
 	}
@@ -41,7 +53,13 @@ class App extends React.Component {
 	
 	componentDidMount() {
 		this.loadFromStorage();
-	}
+  }
+  
+  setLoadingState = (loading) => {
+		this.setState({
+			loading:loading
+    })
+  }
   
   // REST - edit toistaiseksi puuttuu
 
@@ -49,40 +67,75 @@ class App extends React.Component {
 		let request = {
 			method:"GET",
 			mode:"cors",
-			headers:{"Content-type":"application/json"}			
-		}
+			headers:{"Content-type":"application/json", "token":this.state.token}			
+    }
+    this.setLoadingState(true);
 		fetch("/api/food",request).then(response => {
+      this.setLoadingState(false);
 			if(response.ok) {
 				response.json().then(data => {
 					this.setState({
-						list:data
+						foodList:data
 					})
-					this.saveToStorage();
+          this.saveToStorage();
+          console.log(data);
 				}).catch(error => {
-					console.log("Failed to parse JSON data:",error);
+					console.error("Failed to parse JSON data:",error);
 				})
 			} else {
 				console.log("Server responded with status:",response.status);
 			}
 		}).catch(error => {
-			console.log("Server responded with an error:",error);
+      this.setLoadingState(false);
+			console.error("Server responded with an error:",error);
 		})
-	}
+  }
+  
+  getMeals = () => {
+    let request = {
+			method:"GET",
+			mode:"cors",
+      headers:{"Content-type":"application/json", "token":this.state.token, "mealDate":this.state.date.toISOString()}
+      
+    }
+    fetch("/api/diary",request).then(response => {
+      this.setLoadingState(false);
+			if(response.ok) {
+				response.json().then(data => {
+					this.setState({
+						mealList:data
+					})
+          this.saveToStorage();
+          //console.log(data);
+				}).catch(error => {
+					console.error("Failed to parse JSON data:",error);
+				})
+			} else {
+				console.log("Server responded with status:",response.status);
+			}
+		}).catch(error => {
+      this.setLoadingState(false);
+			console.error("Server responded with an error:",error);
+		})
+  }
   
 	removeFoodFromList = (id) => {
 		let request = {
 			method:"DELETE",
 			mode:"cors",
-			headers:{"Content-type":"application/json"}
-		}
+			headers:{"Content-type":"application/json", "token":this.state.token}
+    }
+    this.setLoadingState(true);
 		fetch("/api/food/"+id,request).then(response => {
-			if(response.ok) {
-				console.log("food removed:", {id});
+			this.setLoadingState(false);
+      if(response.ok) {
+				console.log("Food removed:", {id});
 			} else {
 				console.log("Server responded with status:",response.status);
 			}
 		}).catch(error => {
-			console.log(error);
+      this.setLoadingState(false);
+			console.error(error);
 		})
 	}
 
@@ -90,10 +143,12 @@ class App extends React.Component {
 		let request = {
 			method:"POST",
 			mode:"cors",
-			headers:{"Content-type":"application/json"},
+			headers:{"Content-type":"application/json", "token":this.state.token},
 			body:JSON.stringify(food)
-		}
+    }
+    this.setLoadingState(true);
 		fetch("/api/food",request).then(response => {
+      this.setLoadingState(false);
 			if(response.ok) {
         console.log("food added:", {food});
         this.props.history.push("/addmeal");
@@ -101,10 +156,57 @@ class App extends React.Component {
 				console.log("Server responded with status:",response.status);
 			}
 		}).catch(error => {
-			console.log(error);
+      this.setLoadingState(false);
+			console.error(error);
 		})
   }
   
+  addMeal = (meal) => {
+    let newMeal = {"owner": this.state.username, "date": this.state.date, "amount": meal.amount, "food": meal.food}
+    //console.log(newMeal);
+    let request = {
+			method:"POST",
+			mode:"cors",
+			headers:{"Content-type":"application/json", "token":this.state.token},
+			body:JSON.stringify(newMeal)
+    }
+    //console.log(request);
+    this.setLoadingState(true);
+		fetch("/api/meal",request).then(response => {
+      this.setLoadingState(false);
+			if(response.ok) {
+        console.log("meal added:", {newMeal});
+        this.props.history.push("/diary");
+			} else {
+				console.log("Server responded with status:",response.status);
+			}
+		}).catch(error => {
+      this.setLoadingState(false);
+			console.error(error);
+		})
+  }
+
+  removeMeal = (id) => {
+    let request = {
+			method:"DELETE",
+			mode:"cors",
+			headers:{"Content-type":"application/json", "token":this.state.token}
+    }
+    this.setLoadingState(true);
+		fetch("/api/meal/"+id,request).then(response => {
+			this.setLoadingState(false);
+      if(response.ok) {
+        console.log("Meal removed:", {id});
+        
+			} else {
+				console.log("Server responded with status:",response.status);
+			}
+		}).catch(error => {
+      this.setLoadingState(false);
+			console.error(error);
+		})
+  }
+
   onLogin = (user) => {
 		let request = {
 			method:"POST",
@@ -112,27 +214,27 @@ class App extends React.Component {
 			headers:{"Content-type":"application/json"},
 			body:JSON.stringify(user)
     }
-    
+    this.setLoadingState(true);
 		fetch("/login",request).then(response => {
-			//this.setLoadingState(false);
+			this.setLoadingState(false);
 			if(response.ok) {
 				response.json().then(data => {
 					this.setState({
             token:data.token,
             username:data.username,
-						isLogged:true
+            isLogged:true
 					}, () => {
 						this.saveToStorage();
 					}) 
 				}).catch(error => {
-					console.log(error)
+					console.error(error)
         })
 			} else {
 				console.log("Server responded with status:",response.status);
 			}
 		}).catch(error => {
-			//this.setLoadingState(false);
-			console.log(error);
+			this.setLoadingState(false);
+			console.error(error);
     })
     
 	}
@@ -144,7 +246,7 @@ class App extends React.Component {
 			headers: {"Content-type":"application/json",
 			"token":this.state.token}
 		}
-		
+    this.setLoadingState(true);
 		fetch("/logout",request).then(response => {
 			this.setState({
         username:"",
@@ -153,19 +255,25 @@ class App extends React.Component {
 			})
 			sessionStorage.removeItem("state");
 		}).catch(error => {
-			//this.setLoadingState(false);
-			console.log(error);
+			this.setLoadingState(false);
+			console.error(error);
     })
-    
 	}
 
+  onDateChange = (offset) => {
+    let tempDate = this.state.date;
+    tempDate.setDate(this.state.date.getDate()+offset);
+    this.setState({date:tempDate});
+    this.saveToStorage();
+  }
+
   render() {
+    console.log(this.state.foodList);
     return(
       <div className="App">
         <NavBar username={this.state.username} isLogged={this.state.isLogged}
         onLogout={this.onLogout} />
         <Container id="main-content" className="mt-3">
-          
           <Switch>
             <Route exact path="/" render={() =>(
               this.state.isLogged ?
@@ -174,12 +282,12 @@ class App extends React.Component {
             )}/>
             <Route path="/diary" render={() => (
               this.state.isLogged ?
-              (<DayView />):
+              (<DayView onDateChange={this.onDateChange} removeMeal={this.removeMeal} getMeals={this.getMeals} meals={this.state.mealList} date={this.state.date} />):
               (<Redirect to="/"/>)
             )}/>
             <Route path="/addmeal" render={() => (
               this.state.isLogged ?
-              (<AddMeal />):
+              (<AddMeal getFoodList = {this.getFoodList} foodList={this.state.foodList} addMeal={this.addMeal} />):
               (<Redirect to="/"/>)
             )}/>
             <Route path="/addfood" render={() => (
--- frontend/src/components/DayView.js
+++ frontend/src/components/DayView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
+//import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
@@ -10,80 +10,58 @@ export default class DayView extends React.Component {
   constructor(props){
     super(props);
   }
-
+  
+  componentDidMount() {
+    this.props.getMeals();
+  }
+  
   render(){
 
-    //data binding puuttuu vielä kokonaan niin tää ei toimi yhtään
-
-    /* let diaryRow = this.props.meal.map(meal => 
-      <Row key={meal.id} className="diary-item p-1 pl-4">
-        <Col md={3} className="cell">{meal.food.manufacturer}</Col>
-        <Col md={1} className="cell">{meal.amount}</Col>
-        <Col md={1} className="cell">{meal.food.energy}</Col>
-        <Col md={1} className="cell">{meal.food.carbs}g</Col>
-        <Col md={1} className="cell">{meal.food.fat}g</Col>
-        <Col md={1} className="cell">{meal.food.protein}g</Col>
-        <Col md={1}><a href="#" class="text-danger font-weight-bold">X</a></Col>
+    let diaryRow = this.props.meals.map(meal => 
+      <Row key={meal.id} id={meal.id} className="diary-item p-1 pl-4">
+        <Col md={3} className="cell">{meal.food.manufacturer} {meal.food.description}</Col>
+        <Col md={1} className="cell">{meal.amount} g</Col>
+        <Col md={1} id="energy" className="cell pr-2">{meal.food.energy*(meal.amount/100)}</Col>
+        <Col md={1} id="carbs" className="cell">{meal.food.carbs*(meal.amount/100)} g</Col>
+        <Col md={1} id="fat" className="cell">{meal.food.fat*(meal.amount/100)} g</Col>
+        <Col md={1} id="protein" className="cell">{meal.food.protein*(meal.amount/100)} g</Col>
+        <Col md={1}><a href="#" onClick={() => this.props.removeMeal(meal.id)} className="text-danger font-weight-bold">X</a></Col>
       </Row>
-    ) */
-    
+    )
 
     return(
       <div>
         <Row id="date-button-row" className="pl-4 py-1 text-light">
             <Col md={3}></Col>
               <div className="btn-group">
-                <Button className="bg-header text-light">&lt;</Button>
+                <Button className="bg-header text-light" onClick={()=>this.props.onDateChange(-1)}>&lt;</Button>
               </div>
             <Col md={4} id="date" className="align-self-center text-center p-2 mx-1 bg-header">
-              10.6.2020
+              {this.props.date.toLocaleDateString("fi-FI")}
             </Col>
-            <Button className="bg-header text-light">&gt;</Button>
+            <Button className="bg-header text-light" onClick={()=>this.props.onDateChange(1)}>&gt;</Button>
         </Row>
         <Row id="diary-headings" className="py-1 pl-4 font-weight-bold bg-header text-light rounded-top">
           <Col md={3}>Name</Col>
           <Col md={1}>Weight</Col>
-          <Col md={1}>Kcal</Col>
+          <Col md={1} className="pr-2">Energy</Col>
           <Col md={1}>Carbs</Col>
           <Col md={1}>Fat</Col>
           <Col md={1}>Protein</Col>
           <Col md={4}></Col>
         </Row>
-        {/* {diaryRow} */}
+        {diaryRow}
         <Row className="diary-item p-1 pl-4">
           <Col md={3} className="cell"><Link to="/addmeal">Add Item</Link></Col><Col md={9} className="cell"></Col>
         </Row>
         <Row id="diary-total-numbers" className="py-1 pl-4 font-weight-bold bg-header text-light">
           <Col md={1} className="offset-3">Total</Col>
-          <Col md={1}>605</Col>
-          <Col md={1}>44g</Col>
-          <Col md={1}>9,9g</Col>
-          <Col md={1}>82,1g</Col>
+          <Col md={1} className="pr-2">{this.props.meals.reduce((a, v) => a + v.food.energy*(v.amount/100), 0)} kJ</Col>
+          <Col md={1}>{this.props.meals.reduce((a, v) => a + v.food.carbs*(v.amount/100), 0)} g</Col>
+          <Col md={1}>{this.props.meals.reduce((a, v) => a + v.food.fat*(v.amount/100), 0)} g</Col>
+          <Col md={1}>{this.props.meals.reduce((a, v) => a + v.food.protein*(v.amount/100), 0)} g</Col>
         </Row>
       </div>
     )
   }
 }
-
-
-{/* <Row id="diary-headings" class="row py-1 pl-4 font-weight-bold bg-header text-light rounded-top">
-<Col class="col-3">Name</Col><Col class="col-1">Weight</Col><Col class="col-1">Kcal</Col><Col class="col-1">Carbs</Col><Col class="col-1">Fat</Col><Col class="col-1">Protein</Col><Col class="col-4"></Col>
-</Row>
-<Row class="row diary-item p-1 pl-4">
-<div class="col-3 cell">{manufacturer}</div><div class="col-1 cell">500g</div><div class="col-1 cell">310</div><div class="col-1 cell">22,5g</div><div class="col-1 cell">4,5g</div><div class="col-1 cell">42,5g</div><div class="col-1"><a href="#" class="text-danger font-weight-bold">X</a></div>
-</Row>
-<Row class="row diary-item p-1 pl-4 bg-light">
-<div class="col-3 cell">Profeel Proteiinivanukas Suklaa-Toffee</div><div class="col-1 cell">360g</div><div class="col-1 cell">295</div><div class="col-1 cell">21,6g</div><div class="col-1 cell">5,4g</div><div class="col-1 cell">39,6g</div><div class="col-1"><a href="#" class="text-danger font-weight-bold">X</a></div>
-</Row>
-<div class="row diary-item p-1 pl-4">
-<div class="col-3 cell"><a href="#">Add Item</a></div><div class="col-9 cell"></div>
-</div>
-<div id="diary-total-numbers" class="row py-1 pl-4 font-weight-bold bg-header text-light ">
-<div class="offset-3 col-1">Total</div><div class="col-1 ">605</div><div class="col-1">44g</div><div class="col-1">9,9g</div><div class="col-1">82,1g</div>
-</div>
-<!-- <div id="diary-total-goals" class="row py-1 pl-4 font-weight-bold bg-header text-light ">
-<div class="offset-3 col-1">Goal</div><div class="col-1">0</div><div class="col-1">0</div><div class="col-1">0</div><div class="col-1">0</div>
-</div> -->
-<div id="diary-total-titles" class="row py-1 pl-4 font-weight-bold bg-header text-light rounded-bottom ">
-<div class=" offset-4 col-1">Kcal</div><div class="col-1">Carbs</div><div class="col-1">Fat</div><div class="col-1">Protein</div>
-</div> */}
